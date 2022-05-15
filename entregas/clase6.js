f2ebc00e6f518831806bf8f39341f50e654c5f93
danielmarino91 <danielpibot@gmail.com>
2022-05-15 09:13:20 -0300

    Se arreglo la entrega de la clase 6

--- entregas/clase6.js
+++ entregas/clase6.js
@@ -2,27 +2,28 @@ const express = require("express")
 const Contenedor = require("./clase4.js")
 const fs = require("fs")
 
-const prueba = Contenedor
-console.log(prueba)
+const app = express()
+const PORT = 8080
 
-// const app = express()
-// const PORT = 8080
+app.get('/', (request, response) => {
+    response.send(`<h1 style="color: blue; font-family: Roboto">Bienvenidos al servidor express</h1>`)
+})
 
-// app.get('/', (request, response) => {
-//     response.send(`<h1 style="color: blue; font-family: Roboto">Bienvenidos al servidor express</h1>`)
-// })
+app.get('/productos', (request, response) => {
+    const productos = fs.readFileSync("./productos.txt")
+    response.send(productos.toString())
+})
 
-// app.get('/productos', (request, response) => {
-//     const productos = fs.readFileSync("./productos.txt")
-//     response.send(productos.toString())
-// })
+app.get('/productoRandom', (request, response) => {
+    ;(async ()=> {
+        const contenedor = new Contenedor("productos.txt")
+        const getRandom = await contenedor.getRandom()
+        response.send(JSON.stringify(getRandom))
+        })()
+})
 
-// app.get('/productoRandom', (request, response) => {
-//     response.send(`Producto random`)
-// })
+const server = app.listen(PORT, () => {
+    console.log(`Servidor HTTP escuchando en el puerto ${PORT}`)
+})
 
-// const server = app.listen(PORT, ()=>{
-//     console.log(`Servidor HTTP escuchando en el puerto ${PORT}`)
-// })
-
-// server.on("error", error => console.log(`Error en el servidor: ${error}`))
+server.on("error", error => console.log(`Error en el servidor: ${error}`))
