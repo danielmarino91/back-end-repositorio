6a1758994c10faede8b77029473ad58538c6fe26
danielmarino91 <danielpibot@gmail.com>
2022-06-01 23:42:28 -0300

    Correccion en api.js

--- entregas/clase12/api.js
+++ entregas/clase12/api.js
@@ -1,72 +1,75 @@
 class API {
-    constructor(productos) {
-        this.productos = productos;
+    constructor(producto) {
+        this.productos = producto
     }
 
     getProduct(req, res) {
-        const producto = this.productos.find(elem => elem.id === Number(req.params.id))
-        const prodIndex = this.productos.findIndex(elem => elem.id === Number(req.params.id))
-        console.log(prodIndex);
+        const producto = this.productos.find(e => e.id === Number(req.params.id))
 
-        if (producto) {
-            res.json({ producto })
+        if (!producto) {
+            return res.status(400).json({ Error: "Producto no encontrado." })
         } else {
-            res.status(404).json({ error: "Producto no encontrado" })
+            return res.json({ producto })
         }
     }
 
     getAll(req, res) {
-        res.json({ productos: this.productos });
+        return res.json({ Productos: this.productos })
     }
 
     addProduct(req, res) {
-        const productoNuevo = req.body;
+        const productoNuevo = req.body
 
-        if (productoNuevo.title && productoNuevo.price && productoNuevo.thumbnail && Object.keys(productoNuevo).length === 3) {
-            const longitud = this.productos.length;
-            longitud ? productoNuevo.id = this.productos[longitud - 1].id + 1 : productoNuevo.id = 1;
-            this.productos.push(productoNuevo);
-            res.redirect(301, '/')
+        if (!isNaN(productoNuevo.price) == false) {
+            return res.json("El precio ingresado no es un número.")
+        } else if (productoNuevo.title && productoNuevo.price && productoNuevo.thumbnail && Object.keys(productoNuevo).length === 3) {
+            const longitudProducto = this.productos.length
+            longitudProducto ? productoNuevo.id = this.productos[longitudProducto - 1].id + 1 : productoNuevo.id = 1
+            this.productos.push(productoNuevo)
+            return res.json(this.productos)
         } else {
-            return res.status(400).send({ error: "Parametros incorrectos" });
+            return res.status(400).json({ Error: "Complete todos los campos." })
         }
     }
 
     modifyProduct(req, res) {
-        const prodMod = req.body;
+        const productoModificado = req.body
 
-        const format = prodMod.title && prodMod.price && prodMod.thumbnail &&
-            Object.keys(prodMod).length === 3 ? true : null;
+        const producto = this.productos.find(e => e.id === Number(req.params.id))
 
-        const prodIndex = this.productos.findIndex(elem => elem.id === Number(req.params.id))
+        const productoIndex = this.productos.findIndex(e => e.id === Number(req.params.id))
 
-        const producto = this.productos.find(elem => elem.id === Number(req.params.id));
+        const formato = Object.keys(productoModificado).length === 3 ? true : null
 
-        if (format && producto) {
-            prodMod.id = this.productos[prodIndex].id;
-            this.productos[prodIndex] = prodMod;
-            return res.send("Producto modificado");
+        if (isNaN(productoModificado.price) == true) {
+            return res.json({ Error: "El ID y el precio tienen que ser números." })
         }
 
         if (!producto) {
-            return res.status(404).send({ error: "Producto no encontrado" })
+            return res.status(404).json({ Error: "Producto no encontrado." })
         }
 
-        if (!format) {
-            res.send({ error: "El formato del producto no es correcto (debe tener un titulo, un precio y un thumbnail" })
+        if (!formato) {
+            return res.json({ Error: "Se tiene que ingresar un titulo, precio e imagen para modificar el producto." })
+        }
+
+        if (formato && producto) {
+            productoModificado.id = this.productos[productoIndex].id
+            this.productos[productoIndex] = productoModificado
+            return res.json("Se modifico el producto.")
         }
     }
 
     deleteProduct(req, res) {
-        const prodIndex = this.productos.findIndex(elem => elem.id === Number(req.params.id));
+        const productoIndex = this.productos.findIndex(e => e.id === Number(req.params.id))
 
-        if (prodIndex < 0) {
-            return res.status(404).send({ error: "Producto no encontrado" })
+        if (productoIndex < 0) {
+            return res.status(400).json({ Error: "Producto no encontrado." })
         }
 
-        this.productos.splice(prodIndex, 1);
-        res.send("Producto eliminado");
+        this.productos.splice(productoIndex, 1)
+        res.json("Producto eliminado.")
     }
 }
 
-module.exports = API;
+module.exports = API
